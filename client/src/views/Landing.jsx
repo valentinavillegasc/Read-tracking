082dea3f400e12f8ebafc0063f22085abc34b3c8
Valentina Villegas <valentinavillegasc@hotmail.com>
2023-12-15 15:36:52 -0500

    landing2

--- client/src/views/Landing.jsx
+++ client/src/views/Landing.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import style from "./Styles/Landing.module.css";
 import down from "../assets/flechaAbajo.png";
-import right from "../assets/flechaDerecha.png";
+import CardsFunctions from "../components/CardsFunctions";
 export default function Landing() {
   return (
-    <div>
+    <div className={style.landing}>
       <NavBar />
       <section className={style.title}>
         <div className={style.titles}>
@@ -19,18 +19,14 @@ export default function Landing() {
       </section>
       <section id="functions">
         <div className={style.description}>
-          <p>
-            Discover all the incredible features that{" "}
-            <strong>Read tracker</strong> has to offer you.
-          </p>
-          <img src={right} alt="row right" className={style.right} />
-        </div>
-        <div className={style.functions}>
-          <div className={style.function}></div>
-          <div className={style.function}></div>
-          <div className={style.function}></div>
-          <div className={style.function}></div>
-          <div className={style.function}></div>
+          <div className={style.description2}>
+            <p>
+              Discover all the incredible features that{" "}
+              <strong>Read tracker</strong> has to offer you.
+            </p>{" "}
+          </div>
+
+          <CardsFunctions />
         </div>
       </section>
     </div>
