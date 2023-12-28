7072d178a16a88d29011b6aa8fa7901413c64fef
Valentina Villegas <valentinavillegasc@hotmail.com>
2023-12-28 12:55:21 -0500

    update book

--- Server/src/routes/bookRouter.js
+++ Server/src/routes/bookRouter.js
@@ -3,6 +3,7 @@ const uploadMiddleware = require("../handlers/uploadMiddleware");
 const createBook = require("../controllers/Book/createBook");
 const getBookById = require("../controllers/Book/getbookbyId");
 const deleteBook = require("../controllers/Book/deleteBook");
+const updateBook = require("../controllers/Book/updateBook");
 
 //! Get book by ID
 bookRouter.get("/:id", async (req, res) => {
@@ -49,6 +50,47 @@ bookRouter.post("/", uploadMiddleware, async (req, res) => {
       userId
     );
     res.status(200).json({ message: "created", data: newBook });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//! Update book
+bookRouter.put("/:id", uploadMiddleware, async (req, res) => {
+  const { id } = req.params;
+  const {
+    title,
+    author,
+    pages,
+    startDate,
+    endDate,
+    gender,
+    format,
+    sinopsis,
+    review,
+    quotes,
+    stars,
+    cover,
+  } = req.body;
+
+  try {
+    const book = await updateBook(
+      id,
+      cover,
+      title,
+      author,
+      pages,
+      startDate,
+      endDate,
+      gender,
+      format,
+      sinopsis,
+      review,
+      quotes,
+      stars
+    );
+
+    res.status(200).json(book);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
