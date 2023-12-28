f060f08ba9bcfbdf7fd309ecdc0df53ad19a95cc
Valentina Villegas <valentinavillegasc@hotmail.com>
2023-12-28 11:20:28 -0500

    get book by id

--- Server/src/routes/bookRouter.js
+++ Server/src/routes/bookRouter.js
@@ -1,8 +1,21 @@
 const bookRouter = require("express").Router();
 const uploadMiddleware = require("../handlers/uploadMiddleware");
 const createBook = require("../controllers/Book/createBook");
+const getBookById = require("../controllers/Book/getbookbyId");
 
-bookRouter.post("/", uploadMiddleware, (req, res) => {
+//! Get book by ID
+bookRouter.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const book = await getBookById(id);
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//! Create book
+bookRouter.post("/", uploadMiddleware, async (req, res) => {
   const {
     title,
     author,
@@ -19,7 +32,7 @@ bookRouter.post("/", uploadMiddleware, (req, res) => {
   } = req.body;
   const { buffer: cover } = req.file;
   try {
-    const newBook = createBook(
+    const newBook = await createBook(
       cover,
       title,
       author,
