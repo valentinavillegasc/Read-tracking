249470b7a8163cde8e09a81acb45b52032336633
Valentina Villegas <valentinavillegasc@hotmail.com>
2024-01-09 11:03:27 -0500

    functional login

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -1,8 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Styles/Login.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import validation from "../validations";
+import axios from "../utils/axiosConf";
 
 export default function Login() {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState({
+    email: "",
+    password: "",
+  });
+  const [errorLogin, setErrorLogin] = useState("");
+
+  const handleChange = (event) => {
+    const property = event.target.name;
+    const value = event.target.value;
+    setForm({ ...form, [property]: value });
+    setError(validation({ ...form, [property]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(form);
+
+    try {
+      const response = await axios.post("/user/login", form);
+      console.log("Response:", response);
+
+      const user = response.data;
+
+      if (user) {
+        localStorage.setItem("idSession", user.userId);
+        localStorage.setItem("fullname", user.fullname);
+        localStorage.setItem("email", user.email);
+        localStorage.setItem("password", user.password);
+
+        localStorage.setItem(
+          "userData",
+          JSON.stringify({
+            fullname: user.fullname,
+            email: user.email,
+            password: user.password,
+          })
+        );
+
+        navigate("/library");
+      } else {
+        console.log("User data not found in the response");
+      }
+    } catch (error) {
+      console.log("Error:", error);
+      setErrorLogin(error);
+    }
+  };
+
   return (
     <div className={style.login}>
       <div className={style.contenedorLogin}>
@@ -11,17 +67,34 @@ export default function Login() {
         <form className={style.form}>
           <div className={style.formGroup}>
             <label htmlFor="">Email</label>
-            <input type="email" placeholder="example@mail.co" />
+            <input
+              type="email"
+              name="email"
+              placeholder="example@mail.co"
+              onChange={handleChange}
+              value={form.email}
+            />
+            {error.email && (
+              <p className=" text-redError text-xs py-1 m-0">{error.email}</p>
+            )}
           </div>
 
           <div className={style.formGroup}>
             <label htmlFor="">Password</label>
-            <input type="password" placeholder="************" />
+            <input
+              type="password"
+              name="password"
+              placeholder="************"
+              onChange={handleChange}
+              value={form.password}
+            />
+            {errorLogin && errorLogin && (
+              <p className=" text-redError text-xs py-1 m-0">{errorLogin}</p>
+            )}
           </div>
         </form>
-        <Link to="/library">
-          <button>Login</button>
-        </Link>
+
+        <button onClick={handleSubmit}>Login</button>
 
         <div>
           <Link className={style.link}>
